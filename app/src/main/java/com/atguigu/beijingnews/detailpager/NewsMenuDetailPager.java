7300f6afe7b3c8c28f72efacb6edcc55cecb82e4
guo519300884 <519300884@qq.com>
2017-02-06 21:30:54 +0800

    9.实现开源项目ViewPagerIndicator引用和改样式

--- app/src/main/java/com/atguigu/beijingnews/detailpager/NewsMenuDetailPager.java
+++ app/src/main/java/com/atguigu/beijingnews/detailpager/NewsMenuDetailPager.java
@@ -9,6 +9,7 @@ import android.view.ViewGroup;
 import com.atguigu.beijingnews.R;
 import com.atguigu.beijingnews.base.MenuDetailBasePager;
 import com.atguigu.beijingnews.bean.NewsCenterBean;
+import com.viewpagerindicator.TabPageIndicator;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,6 +28,8 @@ public class NewsMenuDetailPager extends MenuDetailBasePager {
 
     @InjectView(R.id.viewpager)
     ViewPager viewpager;
+    @InjectView(R.id.indicator)
+    TabPageIndicator indicator;
     //页签页面集合
     private ArrayList<TabDetailPagers> tabDetailPagers;
 
@@ -39,7 +42,7 @@ public class NewsMenuDetailPager extends MenuDetailBasePager {
     public View initView() {
         //新闻详情页面的视图
         View view = View.inflate(mContext, R.layout.news_menu_detail_pager, null);
-        ButterKnife.inject(this,view);
+        ButterKnife.inject(this, view);
         return view;
     }
 
@@ -49,14 +52,25 @@ public class NewsMenuDetailPager extends MenuDetailBasePager {
 
         //准备数据
         tabDetailPagers = new ArrayList<>();
-        for (int i = 0;i<childrenData.size();i++){
-            tabDetailPagers.add(new TabDetailPagers(mContext,childrenData.get(i)));
+        //根据数据的数量创建相应数量的TabDetailPager,并且将数据传入到对象中
+        for (int i = 0; i < childrenData.size(); i++) {
+            tabDetailPagers.add(new TabDetailPagers(mContext, childrenData.get(i)));
         }
         //设置适配器
         viewpager.setAdapter(new MyPagerAdapter());
+
+        //设置适配器后，监听页面的变化 用TabPagerIndicator
+        indicator.setViewPager(viewpager);
+
     }
 
     private class MyPagerAdapter extends PagerAdapter {
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return childrenData.get(position).getTitle();
+        }
+
         @Override
         public int getCount() {
             return tabDetailPagers.size();
@@ -75,7 +89,7 @@ public class NewsMenuDetailPager extends MenuDetailBasePager {
         @Override
         public Object instantiateItem(ViewGroup container, int position) {
             TabDetailPagers tabDetailPager = tabDetailPagers.get(position);
-            tabDetailPager.initData();
+            tabDetailPager.initData();//重点记忆
             View rootView = tabDetailPager.rootView;
             container.addView(rootView);
             return rootView;
