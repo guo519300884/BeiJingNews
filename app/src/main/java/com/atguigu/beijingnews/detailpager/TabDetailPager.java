edc9961a942c11cfbc393c2ffcfd6301d62138af
guo519300884 <519300884@qq.com>
2017-02-07 17:32:08 +0800

    12.添加顶部轮播页的红色指示点

--- app/src/main/java/com/atguigu/beijingnews/detailpager/TabDetailPager.java
+++ app/src/main/java/com/atguigu/beijingnews/detailpager/TabDetailPager.java
@@ -17,6 +17,7 @@ import com.atguigu.beijingnews.base.MenuDetailBasePager;
 import com.atguigu.beijingnews.bean.NewsCenterBean;
 import com.atguigu.beijingnews.bean.TabDetailPagerBean;
 import com.atguigu.beijingnews.utils.Constants;
+import com.atguigu.beijingnews.utils.DensityUtil;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.google.gson.Gson;
@@ -51,6 +52,7 @@ public class TabDetailPager extends MenuDetailBasePager {
     private TabDetailPagerAdapter adapter;
     //顶部轮播图片新闻的数据
     private List<TabDetailPagerBean.DataEntity.TopnewsBean> topnews;
+    private int prePosition;
 
     public TabDetailPager(Context context, NewsCenterBean.DataBean.ChildrenBean childrenBean) {
         super(context);
@@ -125,7 +127,29 @@ public class TabDetailPager extends MenuDetailBasePager {
         //设置监听 ViewPager的页面变化
         viewpager.addOnPageChangeListener(new MyOnPageChangeListener());
         //设置标题
-        tvTitle.setText(topnews.get(0).getTitle());
+        tvTitle.setText(topnews.get(prePosition).getTitle());
+
+        //删除指示点缓存
+        llGroupPoint.removeAllViews();
+
+        //添加红指示点
+        for (int i = 0;i < topnews.size();i++){
+            //添加到线性布局
+            ImageView point = new ImageView(mContext);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(-2,ViewGroup.LayoutParams.WRAP_CONTENT);
+            if(i != 0) {
+                //设置距离左边的间距
+                params.leftMargin = DensityUtil.dip2px(mContext,7);
+                point.setEnabled(false);
+            }else {
+                point.setEnabled(true);
+            }
+            point.setLayoutParams(params);
+            //设置指示点的背景选择器
+            point.setBackgroundResource(R.drawable.point_selector);
+
+            llGroupPoint.addView(point);
+        }
 
     }
 
@@ -133,7 +157,11 @@ public class TabDetailPager extends MenuDetailBasePager {
     private class MyOnPageChangeListener implements ViewPager.OnPageChangeListener {
         @Override
         public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
+            //非当前页的指示点保持默认
+            llGroupPoint.getChildAt(prePosition).setEnabled(false);
+            //当前页的指示点设置高亮
+            llGroupPoint.getChildAt(position).setEnabled(true);
+            prePosition = position;
         }
 
         @Override
