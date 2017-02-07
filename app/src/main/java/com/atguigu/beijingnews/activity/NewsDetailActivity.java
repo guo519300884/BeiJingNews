82955de9e79ef775c086c27a66a6edfaac2dd582
guo519300884 <519300884@qq.com>
2017-02-07 21:48:05 +0800

    18.实现webView设置选择字体大小

--- app/src/main/java/com/atguigu/beijingnews/activity/NewsDetailActivity.java
+++ app/src/main/java/com/atguigu/beijingnews/activity/NewsDetailActivity.java
@@ -1,6 +1,8 @@
 package com.atguigu.beijingnews.activity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.webkit.WebSettings;
@@ -36,7 +38,15 @@ public class NewsDetailActivity extends AppCompatActivity {
     @InjectView(R.id.activity_news_detail)
     LinearLayout activityNewsDetail;
 
+    //加载新闻页的url
     private String url;
+    //设置字体大小 默认正常大小
+    private int currentTextSize = 2;
+    //临时设置字体大小
+    private int tempTextSize = 2;
+
+    private WebSettings setting;
+    private WebSettings webSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,13 +64,17 @@ public class NewsDetailActivity extends AppCompatActivity {
         webview.loadUrl(url);
 //      webview.loadUrl("http://www.baidu.com");
 
-        WebSettings webSettings = webview.getSettings();
+        webSettings = webview.getSettings();
+
         //支持JavaScript 脚本语言
         webSettings.setJavaScriptEnabled(true);
+
         //添加缩放按钮 页面支持
         webSettings.setBuiltInZoomControls(true);
+
         //支持双击变换大小 页面支持
         webSettings.setUseWideViewPort(true);
+
         //设置监听
         webview.setWebViewClient(new WebViewClient() {
             @Override
@@ -78,10 +92,56 @@ public class NewsDetailActivity extends AppCompatActivity {
                 finish();
                 break;
             case R.id.ib_textsize:
-
+                showChangeTextSizeDialog();
                 break;
             case R.id.ib_share:
 
+                break;
+        }
+    }
+
+    private void showChangeTextSizeDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("选择字体大小");
+        String items[] = {"超大号字体", "大号字体", "正常字体", "小号字体", "超小号字体"};
+        builder.setSingleChoiceItems(items, currentTextSize,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        tempTextSize = which;
+//                        currentSelectTextSize = which;
+                    }
+                });
+        builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                currentTextSize = tempTextSize;
+                changeTextSize(currentTextSize);
+            }
+        });
+        builder.setNegativeButton("取消", null);
+        builder.show();
+    }
+
+    //设置字体大小
+    protected void changeTextSize(int currentTextSize) {
+        switch (currentTextSize) {
+            case 0://超大
+                webSettings.setTextSize(WebSettings.TextSize.LARGEST);
+                break;
+            case 1://大
+                webSettings.setTextSize(WebSettings.TextSize.LARGER);
+                break;
+            case 2://正常
+                webSettings.setTextSize(WebSettings.TextSize.NORMAL);
+                break;
+            case 3://小
+                webSettings.setTextSize(WebSettings.TextSize.SMALLER);
+                break;
+            case 4://超小
+                webSettings.setTextSize(WebSettings.TextSize.SMALLEST);
+                break;
+            default:
                 break;
         }
     }
